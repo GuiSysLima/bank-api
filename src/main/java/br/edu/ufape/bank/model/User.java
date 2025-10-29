6ed42d587d4b9ee4df8a5fc35123d6bab3577d62
Guilherme Felix <guilhermerutemfelix@gmail.com>
2025-10-29 12:57:13 -0300

    feat(model): cpf & password to user

--- src/main/java/br/edu/ufape/bank/model/User.java
+++ src/main/java/br/edu/ufape/bank/model/User.java
@@ -1,26 +1,53 @@
 package br.edu.ufape.bank.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    @Column(nullable = false)
     private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @NotBlank(message = "CPF cannot be blank")
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Column(nullable = false)
+    private String password;
+
 
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+        this.cpf = cpf;
+        this.password = password;
     }
 }
