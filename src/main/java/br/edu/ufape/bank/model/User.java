f5212ccd4704e7654e0c20d1b9e6d09a6fe713d3
Guilherme Felix <guilhermerutemfelix@gmail.com>
2025-10-29 13:02:54 -0300

    feat(model): user relation to account

--- src/main/java/br/edu/ufape/bank/model/User.java
+++ src/main/java/br/edu/ufape/bank/model/User.java
@@ -1,10 +1,14 @@
 package br.edu.ufape.bank.model;
 
+import java.util.Set;
+import java.util.HashSet;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -42,6 +46,13 @@ public class User {
     @Size(min = 8, message = "Password must be at least 8 characters long")
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(
+        mappedBy = "user",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
+    private Set<Account> accounts = new HashSet<>();
 
 
     public User(String name, String email) {
