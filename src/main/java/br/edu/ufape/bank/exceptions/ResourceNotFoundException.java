6be15254227fc13c971fd904458f9d747819bc82
Guilherme Felix <guilherme.flima@ufape.edu.br>
2025-11-21 11:35:25 -0300

    refactor(exception): resource not found

--- src/main/java/br/edu/ufape/bank/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/edu/ufape/bank/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package br.edu.ufape.bank.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     
     public ResourceNotFoundException(String message) {
